b90c60093b4fee77e7747c023bc838c9a5f6fcbb
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2024-01-14 01:19:59 +0530

    Add project files.

---
+++ AzureCacheRedis/Program.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+
+string connectionString = "sandeep-cache.redis.cache.windows.net:6380,password=phGh4eb1LLc6WzDSqXoaUAvjXV9dcuEIbAzCaOI4Ljg=,ssl=True,abortConnect=False";
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(connectionString);
+
+SetCacheData();
+//GetCacheData();
+
+void SetCacheData()
+{
+    IDatabase database=redis.GetDatabase();
+
+    database.StringSet("top:3:subjects", "Maths,Chemistry,Physics");
+
+    Console.WriteLine("Cache data set");
+}
+
+void GetCacheData()
+{
+    IDatabase database = redis.GetDatabase();
+    if (database.KeyExists("top:3:subjects"))
+        Console.WriteLine(database.StringGet("top:3:subjects"));
+    else
+        Console.WriteLine("key does not exist");
+
+}
